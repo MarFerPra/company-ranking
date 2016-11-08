bbf3eef79f77827e8701f733996a854b93fd0da0
MarFerPra <marco.fp94@gmail.com>
2016-11-08 20:53:29 +0100

    Fix for production.

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ var db_url = 'mongodb://localhost/testdb';
 var db_user = process.env.DB_USER || null;
 var db_password = process.env.DB_PASSWORD || null;
 
-if(process.env.NODE_ENV == 'production'){
-  db_url = 'mongo ds147487.mlab.com:47487/company-ranking';
+if(db_user && db_password && process.env.NODE_ENV == 'production'){
+  db_url = 'mongodb://'+ db_user + ':' + db_password + '@ds147487.mlab.com:47487/company-ranking';
 }
 
 mongoose.connect(db_url, {user: db_user, password: db_password}, function(error) {
