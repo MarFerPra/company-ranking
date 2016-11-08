d17fef65d7fd248f166d21106736fdf2b72427a8
MarFerPra <marco.fp94@gmail.com>
2016-11-08 19:00:21 +0100

    Production fixes.

--- index.js
+++ index.js
@@ -12,16 +12,30 @@ var port = 8080;
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/testdb', function(error) {
+var db_url = 'mongodb://localhost/testdb';
+
+
+var db_user = process.env.DB_USER || null;
+var db_password = process.env.DB_PASSWORD || null;
+
+if(process.env.NODE_ENV == 'production'){
+  db_url = 'mongo ds147487.mlab.com:47487/company-ranking';
+}
+
+mongoose.connect(db_url, {user: db_user, password: db_password}, function(error) {
     if (error) {
         console.log("Error connecting to database.");
     } else {
         console.log("Successfully connected to database.");
     }
-    app.listen(port,function(err){
-        if(err) console.log("Server error "+err);
-        else console.log("Server listening at port "+port);
-    });
+    startServer(port);
 });
+
+function startServer(port){
+  app.listen(port,function(err){
+      if(err) console.log("Server error "+err);
+      else console.log("Server listening at port "+port);
+  });
+}
 
         // TODO: Implement Rating operations.
